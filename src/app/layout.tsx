f4cee7f6590e17e5d55519ec18ed4fb3724847e3
Tennovell <tennovell@gmail.com>
2026-07-14 00:54:12 +0900

    Perbaiki import CSS di layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+import './globals.css';
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="id">
-      <body className="bg-[#0a0a0a] m-0 p-0">{children}</body>
+      <body className="bg-gray-50 text-gray-900">{children}</body>
     </html>
   );
 }
